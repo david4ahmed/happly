bcff749b4891ca8668a759a9b94af05906d9d02e
yonghyun-c <yonghyuc@gamil.com>
2019-04-14 01:32:46 -0400

    reflect design

--- src/App.vue
+++ src/App.vue
@@ -1,67 +1,114 @@
 <template>
-  <div class="container">
-    <Header/>
-    <!--<img alt="Vue logo" src="./assets/logo.png">-->
-    <!--<HelloWorld msg="Welcome to Your Vue.js App"/>-->
-    <!--<Contact/>-->
-    <!--<ComfortZone/>-->
-    <!--<ComplimentFriends/>-->
-    <!--<ComplimentUser/>-->
-    <!--<ReflectBehavior/>-->
-    <!--<RandomMedia/>-->
-    <!--<Statistics/>-->
-    <Landing/>
+  <div id="slider" class="slider" @mousemove="mouseMoving" @mouseleave="stopDrag">
+    <div class="slider-cards" :style="`transform: translate3d(${cardsX}px,0,0)`">
+      <div @mousedown="startDrag"
+           @mouseup="stopDrag"
+           class="slider-card">
+        <Main/>
+      </div>
+
+      <div @mousedown="startDrag"
+           @mouseup="stopDrag"
+           class="slider-card">
+        <Feel/>
+      </div>
+
+      <div @mousedown="startDrag"
+           @mouseup="stopDrag"
+           class="slider-card">
+        <Spirit/>
+      </div>
+
+      <div @mousedown="startDrag"
+           @mouseup="stopDrag"
+           class="slider-card">
+        <Mindful/>
+      </div>
+
+    </div>
   </div>
 </template>
 
 <script>
-import Header from "./components/layout/Header.vue";
-import Contact from './components/Contact'
-import ComfortZone from './components/ComfortZone'
-import ComplimentFriends from './components/ComplimentFriends'
-import ComplimentUser from './components/ComplimentUser'
-import ReflectBehavior from './components/ReflectBehavior'
-import RandomMedia from './components/RandomMedia'
-import Statistics from './components/Statistics'
-import Landing from "./components/Landing.vue";
+import tweenlite from 'gsap'
+
+import Main from "./components/Main.vue";
+import Feel from "./components/Feel.vue";
+import Spirit from "./components/Spirit.vue";
+import Mindful from "./components/Mindful.vue";
+import Vue from 'vue'
+import Vuetify from 'vuetify'
+
+Vue.use(Vuetify);
 
 export default {
   name: "app",
   components: {
-    Header,
-    Landing,
-    Contact,
-    ComfortZone,
-    ComplimentFriends,
-    ComplimentUser,
-    ReflectBehavior,
-    Statistics
+    Main,
+    Feel,
+    Spirit,
+    Mindful
+  },
+  data: function () {
+    return {
+      selectedIndex: 0,
+      dragging: false,
+      initialMouseX: 0,
+      initialCardsX: 0,
+      cardsX: 0
+    }
+  },
+  methods: {
+    startDrag (e) {
+      this.dragging = true;
+      this.initialMouseX = e.pageX;
+      this.initialCardsX = this.cardsX;
+    },
+    stopDrag () {
+      this.dragging = false;
+
+      const cardWidth = 330;
+      const nearestSlide = - Math.round(this.cardsX / cardWidth);
+      this.selectedIndex = Math.min(Math.max(0, nearestSlide), 4 - 1);
+      tweenlite.to(this, 0.3, {cardsX: -this.selectedIndex * cardWidth});
+    },
+    mouseMoving (e) {
+      if(this.dragging) {
+        const dragAmount = e.pageX - this.initialMouseX;
+        const targetX = this.initialCardsX + dragAmount;
+        this.cardsX = targetX;
+      }
+    }
   }
-};
+}
 </script>
 
 <style>
-@import url("https://fonts.googleapis.com/css?family=Open+Sans");
-* {
-  box-sizing: border-box;
-  margin: 0;
-  padding: 0;
-}
+  html, body{
+    font-family: 'Lato', sans-serif;
+  }
+  .slider {
+    overflow: hidden;
+    background-color: #1F1140;
+    width: 360px;
+    height: 685px;
+  }
 
-body {
-  font-family: "Open Sans", sans-serif;
-  line-height: 1.6;
-  background-image: url(../public/bckimg.jpg);
-  background-position: center center;
-  background-repeat: no-repeat;
-  background-attachment: fixed;
-  background-size: cover;
-}
-
-.container {
-  max-width: 1100px;
-  margin: auto;
-  overflow: auto;
-  padding: 0 2rem;
-}
+  .slider-cards {
+    position: relative;
+    width: 5000px;
+    margin: 20px 30px;
+    z-index: 1;
+  }
+  .slider-card {
+    display: inline-block;
+    background-color: #e6ffed;
+    overflow: hidden;
+    width: 300px;
+    height: 649px;
+    margin-right: 30px;
+    border-radius: 12px;
+    -webkit-box-shadow: 0px 60px 20px -20px rgba(0, 0, 0, 0.3);
+    box-shadow: 0px 23px 14px -17px rgba(0, 0, 0, 0.3);
+  }
 </style>
---
+++ src/components/Main.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="background">
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Main"
+    }
+</script>
+
+<style scoped>
+    .background {
+        background-image: url("../assets/main.png");
+        background-repeat: no-repeat;
+        background-size: contain;
+        width: 100%;
+        height: 100%;
+    }
+</style>
